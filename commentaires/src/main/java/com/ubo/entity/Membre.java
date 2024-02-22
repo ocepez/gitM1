e1005ddc03e3432207fc0eb525f4f76faa015c47
ocepez <Oceane.Pezennec@etudiant.univ-brest.fr>
2024-02-22 23:53:26 +0100

    Refactor : Ajout Entitées principales

---
+++ commentaires/src/main/java/com/ubo/entity/Membre.java
@@ -0,0 +1,16 @@
+package com.ubo.entity;
+
+
+
+
+
+public class Membre {
+
+    private int id_membre;
+    private String email_membre;
+    private String nom_membre;
+    private String prenom_membre;
+    private int age_membre;
+    private String adresse_membre;
+    private String photo_membre;
+}
