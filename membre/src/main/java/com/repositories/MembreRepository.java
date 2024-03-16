d379bb9a6d76f55b195bbb414159d7297d4ce13b
ocepez <Oceane.Pezennec@etudiant.univ-brest.fr>
2024-03-16 17:01:37 +0100

    Refactor : docker-compose.yml

--- membre/src/main/java/com/repositories/MembreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.repositories;
-
-import com.entities.Membre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface MembreRepository extends JpaRepository<Membre, Integer> {
-}
