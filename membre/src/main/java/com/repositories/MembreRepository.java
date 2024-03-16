20da3f8f7ec1d0563959bb624c7bedd2ddc4334a
ocepez <Oceane.Pezennec@etudiant.univ-brest.fr>
2024-03-16 16:53:48 +0100

    Refactor : dossier Membres -> Membre

---
+++ membre/src/main/java/com/repositories/MembreRepository.java
@@ -0,0 +1,8 @@
+package com.repositories;
+
+import com.entities.Membre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MembreRepository extends JpaRepository<Membre, Integer> {
+}
